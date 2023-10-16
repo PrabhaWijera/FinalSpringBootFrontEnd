2369ded85a6ad15067e125182808ad404e28afed
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-10-16 19:35:13 +0530

    Admin pannel

--- responsive-travel-website-main/user_html/login.js
+++ responsive-travel-website-main/user_html/login.js
@@ -1,24 +1,26 @@
+// Get references to HTML elements
+const loginEmailInput = document.getElementById("email");
+const loginPasswordInput = document.getElementById("password");
+const loginButton = document.getElementById("loginButton");
+
+// Add an event listener to the login button
+loginButton.addEventListener("click", loginUser);
+
 function loginUser() {
-    // Get user input
-    const loginEmail = document.getElementById("email").value;
-    const loginPassword = document.getElementById("password").value;
+    // Retrieve login data from the input fields
+    const loginEmail = loginEmailInput.value;
+    const loginPassword = loginPasswordInput.value;
 
     // Retrieve stored registration data from local storage
     const regEmail = localStorage.getItem("email");
     const regPassword = localStorage.getItem("password");
 
-    // Input validation: Check if fields are not empty
-    if (!loginEmail || !loginPassword) {
-        alert("Please fill in both email and password fields");
-        return;
-    }
-
-    // Check if the entered credentials match the stored data
-    if (loginEmail === regEmail && loginPassword === regPassword) {
-        // Successful login, navigate to the "regular.html" page
-        window.location.href = "regular_p.html";
+    if (!regEmail || !regPassword) {
+        alert("No registration data found. Please sign up.");
+    } else if (loginEmail === regEmail && loginPassword === regPassword) {
+        alert("Successful login");
+        window.location.href = 'regular_p.html';
     } else {
-        // Invalid login, show an error message
         alert("Invalid email or password");
     }
 }
