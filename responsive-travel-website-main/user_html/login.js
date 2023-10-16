0b31a63a02926489340a200b713d7e3c5994170b
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-10-16 18:34:26 +0530

    Admin pannel

--- responsive-travel-website-main/user_html/login.js
+++ responsive-travel-website-main/user_html/login.js
@@ -1,15 +1,22 @@
 function loginUser() {
+    // Get user input
     const loginEmail = document.getElementById("email").value;
     const loginPassword = document.getElementById("password").value;
 
     // Retrieve stored registration data from local storage
     const regEmail = localStorage.getItem("email");
     const regPassword = localStorage.getItem("password");
-    alert("  email or  ");
 
+    // Input validation: Check if fields are not empty
+    if (!loginEmail || !loginPassword) {
+        alert("Please fill in both email and password fields");
+        return;
+    }
+
+    // Check if the entered credentials match the stored data
     if (loginEmail === regEmail && loginPassword === regPassword) {
         // Successful login, navigate to the "regular.html" page
-        window.location.href = "../regular_p.html";
+        window.location.href = "regular_p.html";
     } else {
         // Invalid login, show an error message
         alert("Invalid email or password");
