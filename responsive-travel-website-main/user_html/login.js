143eccab1d6414b80279d0be2475c6b73f25aa1a
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-10-17 00:48:43 +0530

    Admin pannel

--- responsive-travel-website-main/user_html/login.js
+++ responsive-travel-website-main/user_html/login.js
@@ -1,3 +1,5 @@
+/*
+
 // Get references to HTML elements
 const loginEmailInput = document.getElementById("email");
 const loginPasswordInput = document.getElementById("password");
@@ -22,5 +24,59 @@ function loginUser() {
         window.location.href = 'regular_p.html';
     } else {
         alert("Invalid email or password");
+    }
+
+}
+*/
+
+let isLoggedIn = false;
+const loginButton = document.getElementById("loginButton");
+const loginEmailInput = document.getElementById("email");
+const loginPasswordInput = document.getElementById("password");
+const packageButtons = document.querySelectorAll(".package-button");
+
+loginButton.addEventListener("click", loginUser);
+
+packageButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const packageID = button.dataset.packageId;
+        redirectToPackagePage(packageID);
+    });
+});
+
+function loginUser() {
+    const loginEmail = loginEmailInput.value;
+    const loginPassword = loginPasswordInput.value;
+    const regEmail = localStorage.getItem("email");
+    const regPassword = localStorage.getItem("password");
+
+    if (!regEmail || !regPassword) {
+        alert("No registration data found. Please sign up.");
+    } else if (loginEmail === regEmail && loginPassword === regPassword) {
+        alert("Successful login");
+        isLoggedIn = true;
+        window.location.href = 'regular_p.html';
+    } else {
+        alert("Invalid email or password");
+    }
+}
+
+function redirectToPackagePage(packageID) {
+    const packagePages = {
+        "pack1": "regular_p.html",
+        "pack2": "Mid-level_package.html",
+        "pack3": "Luxury.html",
+        "pack4": "Super-Luxury-Package.html"
+    };
+
+    if (isLoggedIn) {
+        const packagePageURL = packagePages[packageID];
+        if (packagePageURL) {
+            window.location.href = packagePageURL;
+        } else {
+            alert("Invalid package selection.");
+        }
+    } else {
+        alert("You must log in before selecting a package.");
     }
 }
