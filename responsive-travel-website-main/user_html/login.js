1ce21a178cc775a16c972ef6d06f2870792fdea6
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-10-17 19:49:17 +0530

    Admin pannel

--- responsive-travel-website-main/user_html/login.js
+++ responsive-travel-website-main/user_html/login.js
@@ -29,12 +29,16 @@ function loginUser() {
 
 }
 */
-// Function to redirect to the appropriate package page based on the package ID
-function redirectToPackage(packageId) {
-    // Get user's email and password from input fields
-    const loginEmailInput = document.getElementById("email");
-    const loginPasswordInput = document.getElementById("password");
 
+const loginEmailInput = document.getElementById("email");
+const loginPasswordInput = document.getElementById("password");
+const loginButton = document.getElementById("loginButton");
+
+// Add an event listener to the login button
+loginButton.addEventListener("click", loginUser);
+
+function loginUser() {
+    // Retrieve login data from the input fields
     const loginEmail = loginEmailInput.value;
     const loginPassword = loginPasswordInput.value;
 
@@ -42,40 +46,48 @@ function redirectToPackage(packageId) {
     const regEmail = localStorage.getItem("email");
     const regPassword = localStorage.getItem("password");
 
-    if (loginEmail === regEmail && loginPassword === regPassword) {
-        // Email and password match, redirect to the appropriate package
-        switch (packageId) {
-            case "pack1":
-                window.location.href = 'regular_p.html';
-                break;
-            case "pack2":
-                window.location.href = 'Mid-level_package.html';
-                break;
-            case "pack3":
-                window.location.href = 'Luxury.html';
-                break;
-            case "pack4":
-                window.location.href = 'Super-Luxury-Package.html';
-                break;
-            default:
-                // Default redirection if no specific conditions match
-                window.location.href = 'default.html';
+    // Retrieve package data from local storage
+    const regular = localStorage.getItem("Regular");
+    const mid = localStorage.getItem("MID");
+    const lux = localStorage.getItem("LuxValue");
+    const s_lux = localStorage.getItem("SuperLux");
+
+    // Ensure that retrieved values are treated as strings (assuming you stored them as strings)
+    const regEmailString = String(regEmail);
+    const regPasswordString = String(regPassword);
+
+    const regularString = String(regular);
+    const midString = String(mid);
+    const luxString = String(lux);
+    const s_luxString = String(s_lux);
+
+    if (!regEmailString || !regPasswordString) {
+        alert("No registration data found. Please sign up.");
+    } else if (loginEmail === regEmailString && loginPassword === regPasswordString) {
+        alert("Successful login");
+
+        console.log("Regular:", regularString);
+        console.log("Mid:", midString);
+        console.log("Lux:", luxString);
+        console.log("SuperLux:", s_luxString);
+
+        if (regularString === "1") {
+            alert(regularString);
+            window.location.href = 'regular_p.html';
+        } else if (midString === "2") {
+            alert(midString);
+            window.location.href = 'Mid-level_package.html';
+        } else if (luxString === "3") {
+            alert(luxString);
+            window.location.href = 'Luxury.html';
+        } else if (s_luxString === "4") {
+            alert(s_luxString);
+            window.location.href = 'Super-Luxury-Package.html';
         }
     } else {
         alert("Invalid email or password");
     }
-}
+    }
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the package buttons
-    const packageButtons = document.querySelectorAll(".package-button");
 
-    // Add click event listeners to the package buttons
-    packageButtons.forEach(button => {
-        button.addEventListener("click", function(event) {
-            event.preventDefault(); // Prevent default form submission (if any)
-            const packageId = this.id; // Get the package ID from the button's id
-            redirectToPackage(packageId);
-        });
-    });
-});
+
