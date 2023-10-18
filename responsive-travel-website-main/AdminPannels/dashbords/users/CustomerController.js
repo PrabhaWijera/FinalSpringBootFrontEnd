d2e00d6aa44accc9e1470bf55dfa63118e74de6d
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-10-18 23:38:19 +0530

    ajax  guide

--- responsive-travel-website-main/AdminPannels/dashbords/users/CustomerController.js
+++ responsive-travel-website-main/AdminPannels/dashbords/users/CustomerController.js
@@ -3,106 +3,129 @@ import { AlertController } from "../AlertController.js";
 import { LocalStorageDB } from "../LocalStorageDB.js";
 
 
+$("#cusAddButton").onclick(function (){
+    saveCustomer();
+});
+$("#cusUpdateButton").onclick(function (){
+    updateCustomer();
+});
+
+// Function to handle image upload
+function uploadImage(file, successCallback, errorCallback) {
+    const data = new FormData();
+    data.append("myFile", file);
+
+    $.ajax({
+        url: "http://localhost:8080/upload", // Replace with the correct upload endpoint
+        method: "POST",
+        async: true,
+        contentType: false,
+        processData: false,
+        data: data,
+        success: successCallback,
+        error: errorCallback,
+    });
+}
 
 // Function to handle guide data save
-// Event handler for saving hotel data
-$("#hotelAddButton").click(function () {
-    saveHotel();
-});
-$("#hUpdateButton").click(function () {
-    updateHotel();
-});
-// Function to save hotel data
-function saveHotel() {
-    const hotelData = {
-        HId: $("#hId").val(),
-        HName: $("#hName").val(),
-        Category: $("#hCategory").val(),
-        Address: $("#hAddress").val(),
-        Gender: $("#hmap").val(),
-        Email: $("#hemail").val(),
-        Contact: $("#hotelContact").val(),
-        Pet: $("#pet").val(),
-        fullBoarddoublehotelFee: $("#FullBoarddoublehotelFee").val(),
-        halfBoardDoublehotelFee: $("#HalfBoardDoublehotelFee").val(),
-        fullBoardTriplehotelFee: $("#FullBoardTriplehotelFee").val(),
-        halfBoardTriplehotelFee: $("#HalfBoardTriplehotelFee").val(),
-        cancellationCriteria: $("#CancellationCriteria").val(),
-        Remark: $("#remark").val(),
+function saveCustomer() {
+    const UserNic_Photo = $("#userNic_Photo")[0].files[0];
+
+
+    const cusData = {
+        GuideIMG: UserNic_Photo.name,
+
+        uId: $("#userid").val(),
+         Name: $("#userName").val(),
+         nic: $("#usernic").val(),
+         password: $("#userpassword").val(),
+        Gender: $("#gender").val(),
+        Age: $("#age").val(),
+        Email: $("#email").val(),
+
+        ContactNumber: $("#contactNumber").val(),
+        Remark: $("#remark").val()
     };
 
     $.ajax({
         url: "http://localhost:8080/save", // Replace with the correct save endpoint
         method: "POST",
-        data: JSON.stringify(hotelData),
+        async: true,
+        data: JSON.stringify(cusData),
         contentType: "application/json",
         success: function (resp) {
             console.log(resp);
-            alert("Hotel data saved successfully");
-            // You can call the uploadImage function here if needed
+            alert(resp.message);
+            uploadImage(UserNic_Photo, function () {
+                alert("Successfully Uploaded");
+            }, function (err) {
+                console.error(err);
+            });
         },
         error: function (error) {
             let parsedError = JSON.parse(error.responseText);
-            alert("Error: " + parsedError.message);
+            alert(parsedError.message);
         },
     });
 }
-//update
-function updateHotel(){
 
-    const hotelData = {
-        HId: $("#huId").val(),
-        HName: $("#huName").val(),
-        Category: $("#huCategory").val(),
-        Address: $("#huAddress").val(),
-        Gender: $("#humap").val(),
-        Email: $("#huemail").val(),
-        Contact: $("#uhotelContact").val(),
-        Pet: $("#upet").val(),
-        fullBoarddoublehotelFee: $("#uFullBoarddoublehotelFee").val(),
-        halfBoardDoublehotelFee: $("#uHalfBoardDoublehotelFee").val(),
-        fullBoardTriplehotelFee: $("#uFullBoardTriplehotelFee").val(),
-        halfBoardTriplehotelFee: $("#uHalfBoardTriplehotelFee").val(),
-        cancellationCriteria: $("#uCancellationCriteria").val(),
-        Remark: $("#uremark").val(),
+//update
+function updateCustomer() {
+    const UserNic_Photo = $("#uuserNic_Photo")[0].files[0];
+
+
+    const cusData = {
+        GuideIMG: UserNic_Photo.name,
+
+        uId: $("#uuserid").val(),
+        Name: $("#uuserName").val(),
+        nic: $("#uusernic").val(),
+        password: $("#uuserpassword").val(),
+        Gender: $("#ugender").val(),
+        Age: $("#uage").val(),
+        Email: $("#uemail").val(),
+
+        ContactNumber: $("#ucontactNumber").val(),
+        Remark: $("#uremark").val()
     };
 
     $.ajax({
         url: "http://localhost:8080/save", // Replace with the correct save endpoint
         method: "PUT",
-        data: JSON.stringify(hotelData),
+        async: true,
+        data: JSON.stringify(cusData),
         contentType: "application/json",
         success: function (resp) {
             console.log(resp);
-            alert("Hotel data update successfully");
-            // You can call the uploadImage function here if needed
+            alert(resp.message);
+            uploadImage(UserNic_Photo, function () {
+                alert("Successfully Uploaded");
+            }, function (err) {
+                console.error(err);
+            });
         },
         error: function (error) {
             let parsedError = JSON.parse(error.responseText);
-            alert("Error: " + parsedError.message);
+            alert(parsedError.message);
         },
     });
 }
 
 
-
-
-// Event handler for deleting hotel data
-$("#deletehotel").click(function () {
-    const hotelID = $("#hId").val(); // Replace with the correct ID field
+$("#Cusdelete").click(function () {
+    const CusID = $("#userid").val();
 
     $.ajax({
         url: 'http://localhost:8080/delete', // Replace with the actual delete endpoint on your server
-        type: 'DELETE',
-        data: { HId: hotelID }, // Send the hotel ID as data
+        type: 'DELETE', // Use DELETE for a delete request
+        data: { CusID: CusID }, // Send the GuideID as data
         success: function (response) {
-            console.log('Hotel data deleted successfully');
-            // You can handle success actions here
+            // Handle success, e.g., show a success message
+            console.log('Data deleted successfully');
         },
         error: function (error) {
-            console.error('Error deleting hotel data:', error);
-            alert("Error deleting hotel data.");
+            // Handle the error, e.g., show an error message
+            console.error('Error deleting data:', error);
         }
     });
 });
-
